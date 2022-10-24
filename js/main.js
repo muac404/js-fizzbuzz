4f78fc2786eb16539e523a645ea364156c0bdc0c
muac404 <muac404@gmail.com>
2022-10-24 21:51:33 +0200

    Aggiunge inserimento dati DOM e stile css

--- js/main.js
+++ js/main.js
@@ -1,28 +1,45 @@
 "use strict"
 
+const boardContainer = document.querySelector('.container')
 
 for (let i = 1; i < 101; i++){
     
     
-    if (i % 3 == 0 && i % 5 == 0){
+    if (i % 3 === 0 && i % 5 === 0){
         let result = "FizzBuzz"
         console.log("FizzBuzz")
+        const boardNumber = document.createElement('div')
+        boardNumber.classList.add('casella')
+        boardNumber.innerHTML = result
+        boardContainer.append(boardNumber)
     }
 
     else if (i % 3 === 0){
         let result = "Fizz"
         console.log("Fizz")
+        const boardNumber = document.createElement('div')
+        boardNumber.classList.add('casella')
+        boardNumber.innerHTML = result
+        boardContainer.append(boardNumber)
     }
 
     else if (i % 5 === 0){
         let result = "Buzz"
         console.log("Buzz")
+        const boardNumber = document.createElement('div')
+        boardNumber.classList.add('casella')
+        boardNumber.innerHTML = result
+        boardContainer.append(boardNumber)
     }
 
 
     else {
         let result = i
         console.log(i)
+        const boardNumber = document.createElement('div')
+        boardNumber.classList.add('casella')
+        boardNumber.innerHTML = result
+        boardContainer.append(boardNumber)
     }   
     
     
