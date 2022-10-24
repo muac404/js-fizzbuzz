f71901c22c4a32b3ae5e327f2d4df99bcbc1d608
muac404 <muac404@gmail.com>
2022-10-24 17:21:24 +0200

    Aggiunge condizioni js

--- js/main.js
+++ js/main.js
@@ -2,5 +2,30 @@
 
 
 for (let i = 1; i < 101; i++){
-    console.log(i)
+     console.log(i)
+
+    if (i % 3 === 0){
+        let result = "Fizz"
+        console.log(result)
+    }
+
+    if (i % 5 === 0){
+        let result = "Buzz"
+        console.log(result)
+    }
+
+    if (i % 3 === 0 && i % 5 === 0){
+        let result = "FizzBuzz"
+        console.log(result)
+    }
+
+
+
+
+    if (i % 3 != 0 || i % 5 != 0){
+        let result = i
+        console.log(result)
+    }
 }
+
+
