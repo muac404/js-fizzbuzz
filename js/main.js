01f4871e4d2cb6afbecdcbeedf53c4af735763e6
muac404 <muac404@gmail.com>
2022-10-24 20:52:40 +0200

    Corregge console.log

--- js/main.js
+++ js/main.js
@@ -2,30 +2,31 @@
 
 
 for (let i = 1; i < 101; i++){
-     console.log(i)
-
-    if (i % 3 === 0){
-        let result = "Fizz"
-        console.log(result)
-    }
-
-    if (i % 5 === 0){
-        let result = "Buzz"
-        console.log(result)
-    }
-
-    if (i % 3 === 0 && i % 5 === 0){
+    
+    
+    if (i % 3 == 0 && i % 5 == 0){
         let result = "FizzBuzz"
-        console.log(result)
+        console.log("FizzBuzz")
+    }
+
+    else if (i % 3 === 0){
+        let result = "Fizz"
+        console.log("Fizz")
+    }
+
+    else if (i % 5 === 0){
+        let result = "Buzz"
+        console.log("Buzz")
     }
 
 
-
-
-    if (i % 3 != 0 || i % 5 != 0){
+    else {
         let result = i
-        console.log(result)
-    }
+        console.log(i)
+    }   
+    
+    
+    
 }
 
 
